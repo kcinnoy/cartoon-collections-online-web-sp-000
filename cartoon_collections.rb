b8443f6454f0cf35b992cd819df876550486523c
Yonnick Brown <25hour24@gmail.com>
2019-01-28 23:47:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.map do |cheese|
+  array.find do |cheese|
     cheese_types.include?(cheese)
   end
 end
