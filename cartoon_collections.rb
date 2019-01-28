4c60b61a87573004674baef25121d771061cac12
Yonnick Brown <25hour24@gmail.com>
2019-01-28 23:17:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,14 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |name, index|
+    puts "#{index+1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |element|
+    new_string = element.capitalize
+    new_string+"!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
